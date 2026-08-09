53a4538e52125db7e3c485c507f149ec415929db
CryingDitto <101262081+CryingDitto@users.noreply.github.com>
2026-08-09 23:25:29 +0900

    [W02] Ditto's solve SWEA 1767, 1949, 1952

---
+++ problems/week02/SWEA_1949/CryingDitto/1949.cpp
@@ -0,0 +1,93 @@
+﻿// 1949. 등산로 조성
+// https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PoOKKAPIDFAUq&
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// inputs
+int sizeN, cutCount;
+int map[8][8];
+bool visited[8][8];
+// left, right, up, down
+int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+// maximum length of path
+int mxLength = -1;
+
+void search(int xPos, int yPos, int curHeight, int length, int isCut)
+{
+    mxLength = max(mxLength, length);
+    visited[xPos][yPos] = true;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int nextX = xPos + dx[i];
+        int nextY = yPos + dy[i];
+
+        if (nextX < 0 || nextX >= sizeN || nextY < 0 || nextY >= sizeN) continue;
+        if (visited[nextX][nextY]) continue;
+
+        int nextHeight = map[nextX][nextY];
+
+        if (nextHeight < curHeight)
+        {
+            // possible to go down
+            search(nextX, nextY, nextHeight, length + 1, isCut);
+        }
+        else if (!isCut)
+        {
+            if (nextHeight - curHeight < cutCount)
+            {
+                // possible to cut and go down
+                // 높이는 높을수록 유리하므로 (현재 높이-1)까지만 깎음
+                search(nextX, nextY, curHeight - 1, length + 1, true);
+            }
+        }
+    }
+    visited[xPos][yPos] = false; // restore to visit
+
+    return;
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    for (int tc = 0; tc < T; tc++)
+    {
+        cin >> sizeN >> cutCount;
+
+        int maxHeight = -1;
+        for (int i = 0; i < sizeN; i++)
+        {
+            for (int j = 0; j < sizeN; j++)
+            {
+                cin >> map[i][j];
+                maxHeight = max(map[i][j], maxHeight);
+            }
+        }
+        // collect highest start x,y points
+        vector<pair<int, int>> startPoints;
+        for (int i = 0; i < sizeN; i++)
+        {
+            for (int j = 0; j < sizeN; j++)
+            {
+                if (map[i][j] == maxHeight)
+                {
+                    startPoints.push_back({ i,j });
+                }
+            }
+        }
+        mxLength = -1;
+        for (auto point : startPoints)
+        {
+            search(point.first, point.second, map[point.first][point.second], 1, false);
+        }
+
+        cout << "#" << tc + 1 << " " << mxLength << "\n";
+    }
+    return 0;
+}
---
+++ problems/week02/SWEA_1952/CryingDitto/1952.cpp
@@ -0,0 +1,53 @@
+﻿// 1952. [모의 SW 역량테스트] 수영장
+// https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PpFQaAQMDFAUq
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int prices[4];// daily, monthly, 3 monthly, yearly prices
+int plan[12]; // usage plan for each month
+int calculate(int month)
+{
+    if (month >= 12)
+        return 0;
+
+    if (plan[month] == 0)
+    {
+        return calculate(month + 1);
+    }
+    // 1월 -> 2월 -> 3월 ... 12월, month가 13이상이 되어서야 리턴값 하나씩 돌아오기 시작
+    int minVal = min({
+        prices[0] * plan[month] + calculate(month + 1),
+        prices[1] + calculate(month + 1),
+        prices[2] + calculate(month + 3) // 3개월 동안 이용 가능하므로 세 달 스킵
+        });
+    return minVal;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int T = 0;
+    cin >> T;
+    for (int tc = 0; tc < T; tc++)
+    {
+        // input: prices
+        for (int i = 0; i < 4; i++)
+        {
+            cin >> prices[i];
+        }
+        // input: plan
+        for (int i = 0; i < 12; i++)
+        {
+            cin >> plan[i];
+        }
+        // compare the result of calculate(0) and yearly price
+        int result = min(calculate(0), prices[3]);
+
+        cout << "#" << tc + 1 << " " << result << "\n";
+    }
+    return 0;
+}
