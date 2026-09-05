b41717e12a96d7db48dc9fe573203a763a3dfb1f
yeoroJeong <140257454+yeoroJeong@users.noreply.github.com>
2026-09-05 20:45:38 +0900

    docs: add runnable Clockwise City preview

---
+++ resources/week06/clockwise_city_btype/user_solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void init(int N)
+{
+    // TODO
+}
+
+void addBuilding(int mID, int mRow, int mCol,
+                 int mHeight, int mWidth,
+                 int mRelRow, int mRelCol)
+{
+    // TODO
+}
+
+int move(int mStartID, int mEndID, int M, int mStopover[])
+{
+    // TODO
+    return -1;
+}
